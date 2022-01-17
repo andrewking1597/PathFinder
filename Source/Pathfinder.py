3fe159cb0bbfe77ceec9a1414874bd91a22b3af0
abk <andrewking1597@gmail.com>
2022-01-17 17:16:56 -0500

    create Source directory

--- Source/Pathfinder.py
+++ Source/Pathfinder.py
@@ -1,6 +1,8 @@
 from Node import Node
 import pygame, sys
+
 pygame.init()
+
 
 class Pathfinder:
     def __init__(self, numCols=24, numRows=24, nodesize=25, screensize=600):
@@ -17,19 +19,16 @@ class Pathfinder:
         self.pathFound = False
         self.canDraw = True
 
-
     def createNodes(self):
         for r in range(self.NUM_ROWS):
             self.grid.append([])
             for c in range(self.NUM_COLS):
                 self.grid[r].append(Node(r=r, c=c, screen=self.SCREEN))
 
-
     def setStartNode(self, row, col):
         self.startNode = self.grid[row][col]
         self.startNode.status = 'start'
         self.startNode.gcost = 0
-
 
     def setEndNode(self, row, col):
         self.endNode = self.grid[row][col]
@@ -39,7 +38,6 @@ class Pathfinder:
         for n in self.grid:
             for m in n:
                 m.set_hcost(self.endNode)
-                
 
     def setWallNode(self, n):
         if n == self.startNode or n == self.endNode:
@@ -49,34 +47,32 @@ class Pathfinder:
 
         return
 
-
     def drawNodes(self):
         # draw nodes
         for row in self.grid:
             for n in row:
                 if n.status == None:
-                    color = (0,0,0)
+                    color = (0, 0, 0)
                 elif n.status == 'red':
-                    color = (200,0,0)
+                    color = (200, 0, 0)
                 elif n.status == 'green':
-                    color = (0,200,0)  
+                    color = (0, 200, 0)
                 elif n.status == 'start':
-                    color = (0,0,200)
+                    color = (0, 0, 200)
                 elif n.status == 'end':
-                    color = (0,0,100)
+                    color = (0, 0, 100)
                 elif n.status == 'white':
-                    color = (220,220,220)
+                    color = (220, 220, 220)
                 else:
-                    color = (200,200,0) # this should never happen and will show up as yellow
+                    color = (200, 200, 0)  # this should never happen and will show up as yellow
 
                 pygame.draw.rect(self.SCREEN, color, (n.X, n.Y, n.SIZE, n.SIZE))
 
         # draw grid lines
         for i in range(int(self.NODE_SIZE), self.SCREEN_SIZE, int(self.NODE_SIZE)):
-            pygame.draw.line(self.SCREEN, (255,255,255), (i, 0), (i, self.SCREEN_SIZE)) # vertical lines
-            pygame.draw.line(self.SCREEN, (255,255,255), (0,i), (self.SCREEN_SIZE, i)) # horizontal lines
+            pygame.draw.line(self.SCREEN, (255, 255, 255), (i, 0), (i, self.SCREEN_SIZE))  # vertical lines
+            pygame.draw.line(self.SCREEN, (255, 255, 255), (0, i), (self.SCREEN_SIZE, i))  # horizontal lines
 
-    
     def chooseNextNode(self):
         # loop through the grid (only looking at 'green' nodes)
         # keep track of min fcost
@@ -93,9 +89,8 @@ class Pathfinder:
                     elif n.fcost < bestChoice.fcost:
                         bestChoice = n
 
-        # if bestChoice is the end node: 
+        # if bestChoice is the end node:
         return bestChoice
-
 
     def selectNode(self, r, c):
 
@@ -112,23 +107,23 @@ class Pathfinder:
         # diag neighbors
         if r > 0:
             if c > 0:
-                diagonals.append(self.grid[r-1][c-1])
+                diagonals.append(self.grid[r - 1][c - 1])
             if c < self.NUM_COLS - 1:
-                diagonals.append(self.grid[r-1][c+1])
+                diagonals.append(self.grid[r - 1][c + 1])
         if r < self.NUM_ROWS - 1:
             if c > 0:
-                diagonals.append(self.grid[r+1][c-1])
+                diagonals.append(self.grid[r + 1][c - 1])
             if c < self.NUM_COLS - 1:
-                diagonals.append(self.grid[r+1][c+1])
+                diagonals.append(self.grid[r + 1][c + 1])
         # edge neighbors
         if r > 0:
-            edges.append(self.grid[r-1][c])
+            edges.append(self.grid[r - 1][c])
         if r < self.NUM_ROWS - 1:
-            edges.append(self.grid[r+1][c])
+            edges.append(self.grid[r + 1][c])
         if c > 0:
-            edges.append(self.grid[r][c-1])
+            edges.append(self.grid[r][c - 1])
         if c < self.NUM_COLS - 1:
-            edges.append(self.grid[r][c+1])
+            edges.append(self.grid[r][c + 1])
 
         # for each diag neighbor: status='green', gcost = currentNode.gcost+14 (if neighbor.gcost is None or is greater than the new gcost), update fcost
         for n in diagonals:
@@ -137,14 +132,14 @@ class Pathfinder:
                 continue
 
             n.status = 'green'
-            newg = self.grid[r][c].gcost + 14 
+            newg = self.grid[r][c].gcost + 14
             if n.gcost == None or n.gcost > newg:
                 n.gcost = newg
                 # update fcost
                 n.update_fcost()
                 # set n.prev to point at the current node
                 n.prev = self.grid[r][c]
-            
+
             # if n is the endNode: call showPath(n)
             if n == self.endNode:
                 self.pathFound = True
@@ -157,7 +152,7 @@ class Pathfinder:
                 continue
 
             n.status = 'green'
-            newg = self.grid[r][c].gcost + 10 
+            newg = self.grid[r][c].gcost + 10
             if n.gcost == None or n.gcost > newg:
                 n.gcost = newg
                 # update fcost
@@ -177,23 +172,21 @@ class Pathfinder:
             n.status = 'yellow'
             self.showPath(n.prev)
 
-
     def getClickedNode(self, mpos):
         """
         mpos is the mouse position as (x,y).
         return the node that contains that point
         """
 
-        c = mpos[0] // 25 
+        c = mpos[0] // 25
         r = mpos[1] // 25
 
         return self.grid[r][c]
-            
 
     def runSim(self):
         self.createNodes()
-        self.setStartNode(2,2)
-        self.setEndNode(20,17)
+        self.setStartNode(2, 2)
+        self.setEndNode(20, 17)
 
         # select start node
         self.selectNode(self.startNode.ROW, self.startNode.COL)
@@ -229,7 +222,6 @@ class Pathfinder:
 
         # Quit
         pygame.quit()
-
 
 
 pf = Pathfinder()
